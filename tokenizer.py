a5b566d500bb796c3fe54e53f74935d07e7052e9
Shangmin Guo <shawnguo.cn@gmail.com>
2024-03-30 13:50:10 +0000

    fixed a bug in tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -10,17 +10,16 @@ class Tokenizer(object):
 
         with open(vocab_file, 'r') as f:
             for i, line in enumerate(f):
-                self.vocab[line.strip()] = i
-                self.idx2token[i] = line.strip()
+                self.vocab[line.strip()] = i + 1
+                self.idx2token[i + 1] = line.strip()
 
-        self.vocab['<eos>'] = len(self.vocab)
         self.idx2token[len(self.vocab)] = ''
+        self.vocab['<eos>'] = len(self.vocab)
 
         self.vocab_size = len(self.vocab)
 
-    def encode(self, text:str) -> List[int]:
-        tokens = text.split()
-        return [self.vocab[token] for token in tokens]
+    def encode(self, token:str) -> int:
+        return self.vocab[token]
 
-    def decode(self, tokens:List[int]) -> str:
-        return ' '.join([self.idx2token[token] for token in tokens])
+    def decode(self, idx:int) -> str:
+        return self.idx2token[idx]
