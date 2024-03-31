13c442cc7c5b885006e3c5cb0aec45a50f53dd2e
Shangmin Guo <shawnguo.cn@gmail.com>
2024-03-31 21:24:55 +0100

    updated configuration

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import random
 import numpy as np
+import tomllib
 import torch
+from munch import Munch, munchify
 
 
 def set_seed(seed):
@@ -8,3 +10,12 @@ def set_seed(seed):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+
+
+def get_global_config(path: str):
+    with open(path, 'rb') as f:
+        config = tomllib.load(f)
+    config = munchify(config)
+    assert config.max_input_length == config.model.block_size, \
+        "max_input_length should be equal to model.block_size"
+    return 
