a6d5318791cfd2d5f7ca9c6c07ec04909e70c4be
Shangmin Guo <shawnguo.cn@gmail.com>
2024-04-08 18:14:54 +0100

    updated tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -8,6 +8,7 @@ class Tokenizer(object):
         self.idx2token = {}
         self.bos_token = '<bos>'
         self.eos_token = '<eos>'
+        self.unk_token = '<unk>'
 
         self.vocab[self.bos_token] = 0
         self.idx2token[0] = ''
@@ -17,16 +18,27 @@ class Tokenizer(object):
                 self.vocab[line.strip()] = i + 1
                 self.idx2token[i + 1] = line.strip()
 
+        self.idx2token[len(self.vocab)] = '\n'
+        self.vocab['\n'] = len(self.vocab)
+
         self.idx2token[len(self.vocab)] = ''
         self.vocab[self.eos_token] = len(self.vocab)
 
-        self.bos_idx = 0
+        self.idx2token[len(self.vocab)] = ''
+        self.vocab[self.unk_token] = len(self.vocab)
+
+        self.bos_idx = self.vocab[self.bos_token]
         self.eos_idx = self.vocab[self.eos_token]
+        self.unk_idx = self.vocab[self.unk_token]
 
         self.vocab_size = len(self.vocab)
 
     def encode(self, token:str) -> int:
-        return self.vocab[token]
+        try:
+            return self.vocab[token]
+        except KeyError:
+            print(f'Unknown token: {token}')
+            return self.unk_idx
 
     def decode(self, idx:int) -> str:
         return self.idx2token[idx]
