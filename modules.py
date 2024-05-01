b116e92fd49485f2a5dab8d73d9b5127be0ad49a
Shangmin Guo <shawnguo.cn@gmail.com>
2024-05-01 21:11:09 +0100

    implemented Mistral modules

--- modules.py
+++ modules.py
@@ -12,12 +12,14 @@ https://github.com/mistralai/mistral-src/
 """
 from dataclasses import dataclass
 import math
-from typing import List
+from typing import List, Optional, Tuple
 
 from simple_parsing.helpers import Serializable
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+
+from model import ModelArgs
 
 
 @dataclass
@@ -26,35 +28,42 @@ class MoeArgs(Serializable):
     num_experts_per_tok: int
 
 
-class NewGELU(nn.Module):
-    """
-    Implementation of the GELU activation function currently in Google BERT
-    repo (identical to OpenAI GPT).
-    Reference: Gaussian Error Linear Units (GELU) paper:
-    https://arxiv.org/abs/1606.08415
-    """
-    def forward(self, x):
-        return 0.5 * x * (
-            1.0 + torch.tanh(
-                math.sqrt(2.0 / math.pi) * (x + 0.044715 * torch.pow(x, 3.0))
-            )
-        )
+# utility functions for Rotary Positional Embedding
+def precompute_freqs_cis(dim: int, end: int, theta: float) -> torch.Tensor:
+    freqs = 1.0 / (
+        theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim)
+    )
+    t = torch.arange(end, device=freqs.device)
+    freqs = torch.outer(t, freqs).float()
+    return torch.polar(torch.ones_like(freqs), freqs)
+
+def apply_rotary_emb(
+    q: torch.Tensor,
+    k: torch.Tensor,
+    freqs_cis: torch.Tensor,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    q_ = torch.view_as_complex(q.float().reshape(*q.shape[:-1], -1, 2))
+    k_ = torch.view_as_complex(k.float().reshape(*k.shape[:-1], -1, 2))
+    freqs_cis = freqs_cis[:, None, :]
+    q_out = torch.view_as_real(q_ * freqs_cis).flatten(-2)
+    k_out = torch.view_as_real(k_ * freqs_cis).flatten(-2)
+    return q_out.to(q.dtype), k_out.to(k.dtype)
 
 
-class CausalSelfAttention(nn.Module):
+class MultiHeadAttention(nn.Module):
     """
-    A vanilla multi-head masked self-attention layer with a projection at the end.
-    It is possible to use torch.nn.MultiheadAttention here but I am including an
-    explicit implementation here to show that there is nothing too scary here.
+    A vanilla multi-head masked self-attention layer with a projection at the
+    end. It is possible to use torch.nn.MultiheadAttention here but I am 
+    including an explicit implementation here to show that there is nothing too 
+    scary here.
     """
 
     def __init__(self, config):
         super().__init__()
-        assert config.n_embd % config.n_head == 0
         # key, query, value projections for all heads, but in a batch
-        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        self.c_attn = nn.Linear(config.embd_dim, 3 * config.head_dim)
         # output projection
-        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj = nn.Linear(config.head_dim, config.embd_dim)
         # regularization
         self.attn_dropout = nn.Dropout(config.attn_pdrop)
         self.resid_dropout = nn.Dropout(config.resid_pdrop)
@@ -66,19 +75,20 @@ class CausalSelfAttention(nn.Module):
                       ).view(1, 1, config.block_size, config.block_size)
         )
         self.n_head = config.n_head
-        self.n_embd = config.n_embd
+        self.embd_dim = config.embd_dim
 
     def forward(self, x):
-        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+        # batch size, sequence length, embedding dimensionality (embd_dim)
+        B, T, C = x.size()
 
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        q, k ,v  = self.c_attn(x).split(self.n_embd, dim=2)
+        q, k ,v  = self.c_attn(x).split(self.head_dim, dim=2)
         # (B, nh, T, hs)
-        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        k = k.view(B, T, self.n_head, self.head_dim).transpose(1, 2)
         # (B, nh, T, hs)
-        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        q = q.view(B, T, self.n_head, self.head_dim).transpose(1, 2)
         # (B, nh, T, hs)
-        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        v = v.view(B, T, self.n_head, self.head_dim).transpose(1, 2)
 
         # causal self-attention; Self-attend:
         # (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
@@ -88,35 +98,81 @@ class CausalSelfAttention(nn.Module):
         att = self.attn_dropout(att)
         y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
         # re-assemble all head outputs side by side
-        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        y = y.transpose(1, 2).contiguous().view(B, T, self.head_dim)
 
         # output projection
         y = self.resid_dropout(self.c_proj(y))
         return y
 
 
-class GPTBlock(nn.Module):
-    """ an unassuming Transformer block """
-
-    def __init__(self, config):
+## Grouped Attention Layer
+class GroupedQueryAttention(nn.Module):
+    """
+    A grouped-query attention layer with a projection at the end. This class can
+    also cover the following cases:
+    - Multi-head attention: set `n_kv_head` equal to `n_head`
+    - Multi-query attention: set `n_kv_head` equal to 1, and `n_head` to the
+        number of query heads
+    - Grouped-query attention: set `n_head` to the number of query heads, and
+        `n_kv_head` to `n_group = n_head // n_kv_head`.
+    """
+    def __init__(self, config: ModelArgs) -> None:
         super().__init__()
-        self.ln_1 = nn.LayerNorm(config.n_embd)
-        self.attn = CausalSelfAttention(config)
-        self.ln_2 = nn.LayerNorm(config.n_embd)
-        self.mlp = nn.ModuleDict(dict(
-            c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd),
-            c_proj  = nn.Linear(4 * config.n_embd, config.n_embd),
-            act     = NewGELU(),
-            dropout = nn.Dropout(config.resid_pdrop),
-        ))
-        m = self.mlp
-        # MLP forward
-        self.mlpf = lambda x: m.dropout(m.c_proj(m.act(m.c_fc(x))))
+        self.config = config
 
-    def forward(self, x):
-        x = x + self.attn(self.ln_1(x))
-        x = x + self.mlpf(self.ln_2(x))
-        return x
+        self.n_head: int = config.n_head
+        self.n_kv_head: int = config.n_kv_head
+        self.head_dim: int = config.head_dim
+
+        self.repeats = self.n_head // self.n_kv_head
+        self.scale = config.head_dim ** -0.5
+
+        self.q_proj = nn.Linear(
+            config.embd_dim, config.n_head * config.head_dim, bias=False
+        )
+        self.k_proj = nn.Linear(
+            config.embd_dim, config.n_kv_head * config.head_dim, bias=False
+        )
+        self.v_proj = nn.Linear(
+            config.embd_dim, config.n_kv_head * config.head_dim, bias=False
+        )
+        self.o_proj = nn.Linear(
+            config.n_heads * config.head_dim, config.embd_dim, bias=False
+        )
+
+        self.p_drop = config.p_drop if config.p_drop is not None else 0.1
+
+        self.register_buffer(
+            "attn_bias",
+            torch.tril(
+                torch.ones(config.block_size, config.block_size)
+            ).view(1, 1, config.block_size, config.block_size)
+        )
+
+    def forward(self, x: torch.Tensor, freq_cis: torch.Tensor) -> torch.Tensor:
+        B, T, _ = x.size()
+
+        q, k, v = self.q_proj(x), self.k_proj(x), self.v_proj(x)
+
+        q = q.view(B, T, self.n_head, self.head_dim)
+        k = k.view(B, T, self.n_kv_head, self.head_dim)
+        v = v.view(B, T, self.n_kv_head, self.head_dim)
+
+        q, k = apply_rotary_emb(q, k, freq_cis)
+        q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)
+
+        k = torch.repeat_interleave(k, repeats=self.repeats, dim=1)
+        v = torch.repeat_interleave(v, repeats=self.repeats, dim=1)
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.masked_fill(
+            self.attn_bias[:, :, :T, :T] == 0, float("-inf")
+        )
+        attn = attn.softmax(dim=-1)
+        attn = F.dropout(attn, self.p_drop, training=self.training)
+        y = (attn @ v).transpose(1, 2).contiguous().reshape(B, T, -1)
+        y = F.dropout(y, self.p_drop, training=self.training)
+        return self.o_proj(y)
 
 
 class MoeLayer(nn.Module):
@@ -124,17 +180,17 @@ class MoeLayer(nn.Module):
         self,
         experts: List[nn.Module],
         gate: nn.Module,
-        args: MoeArgs
+        config: MoeArgs
     ) -> None:
         super().__init__()
         self.experts = nn.ModuleList(experts)
         self.gate = gate
-        self.args = args
+        self.config = config
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         gate_logits = self.gate(input)
         weights, selected_experts = torch.topk(
-            gate_logits, self.args.num_experts_per_tok
+            gate_logits, self.config.num_experts_per_tok
         )
         weights = F.softmax(weights, dim=1, dtype=torch.float).to(input.dtype)
         results = torch.zeros_like(input)
@@ -144,3 +200,83 @@ class MoeLayer(nn.Module):
                 input[batch_idx]
             )
         return results
+
+
+class FeedForward(nn.Module):
+    def __init__(self, config: ModelArgs) -> None:
+        super().__init__()
+        self.h1 = nn.Linear(config.embd_dim, config.hidden_dim, bias=False)
+        self.h2 = nn.Linear(config.hidden_dim, config.embd_dim, bias=False)
+        self.h3 = nn.Linear(config.embd_dim, config.hidden_dim, bias=False)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.h2(F.silu(self.h1(x)) * self.h3(x))
+
+
+class RMSNorm(torch.nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def _norm(self, x):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x):
+        output = self._norm(x.float()).type_as(x)
+        return output * self.weight
+
+
+class GPTBlock(nn.Module):
+    """Transformer block used by GPT-2"""
+
+    def __init__(self, config: ModelArgs):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.embd_dim)
+        self.attn = MultiHeadAttention(config)
+        self.ln_2 = nn.LayerNorm(config.embd_dim)
+        self.ffn = nn.ModuleDict(dict(
+            c_fc    = nn.Linear(config.embd_dim, 4 * config.embd_dim),
+            c_proj  = nn.Linear(4 * config.embd_dim, config.embd_dim),
+            act     = nn.GELU(),
+            dropout = nn.Dropout(config.p_drop),
+        ))
+        m = self.ffn
+        # MLP forward
+        self.mlpf = lambda x: m.dropout(m.c_proj(m.act(m.c_fc(x))))
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlpf(self.ln_2(x))
+        return x
+
+
+class MistralBlock(nn.Module):
+    """Transformer block used by Mistral"""
+    def __init__(self, config: ModelArgs) -> None:
+        super().__init__()
+        self.n_head = config.n_head
+        self.embd_dim = config.embd_dim
+        self.attn = GroupedQueryAttention(config)
+        self.attn_norm = RMSNorm(config.embd_dim, eps=config.norm_eps)
+        self.ffn_norm = RMSNorm(config.embd_dim, eps=config.norm_eps)
+
+        self.ffn: nn.Module
+        if config.moe is not None:
+            self.ffn = MoeLayer(
+                experts=[
+                    FeedForward(config) for _ in range(config.moe.num_experts)
+                ],
+                gate=nn.Linear(
+                    config.embd_dim, config.moe.num_experts, bias=False
+                ),
+                moe_config=config.moe,
+            )
+        else:
+            self.ffn = FeedForward(config)
+
+    def forward(self, x: torch.Tensor, freq_cis: torch.Tensor) -> torch.Tensor:
+       r = self.attn(self.attn_norm(x), freq_cis)
+       h = x + r
+       r = self.ffn(self.ffn_norm(h))
+       return h + r
